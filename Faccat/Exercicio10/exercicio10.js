6a5c7acf4b288616ff4dbfc383c230ba78d90441
Raphael Alves <101127310+Kapphael@users.noreply.github.com>
2022-06-06 12:31:17 -0300

    Update exercicio10.js

--- Faccat/Exercicio10/exercicio10.js
+++ Faccat/Exercicio10/exercicio10.js
@@ -3,7 +3,7 @@ let custoFabrica, distribuidor, impostos, valortotal;
 custoFabrica=parseFloat(prompt("Digite o valor do custo de fabrica do veículo: "))
 distribuidor=parseInt( custoFabrica*0.28)
 impostos=parseFloat(custoFabrica*0.45)
-valortotal=parseFloat(custoFabrica+impostos+impostos)
+valortotal=parseFloat(custoFabrica+impostos+distribuidor)
 alert("Valor de impostos: "+impostos)
 alert("Valor de distribuidor: "+distribuidor)
 alert("O valor final do veículo é: "+valortotal)
